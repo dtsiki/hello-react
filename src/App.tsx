7c7d8e9ac0748fc48d6dd6ec9fc469f6edeb2c4d
Daria Terenteva <d.terenteva@infomaximum.biz>
2026-03-24 18:46:05 +0600

    Update Create React App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h1 className="app-title">Hello React!</h1>
+        <p className="app-text">
+          Это демо-проект для статьи о том, как создать React-приложение и
+          опубликовать его с помощью GitHub Pages
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="app-link"
+          href="https://dtsiki.github.io/blog/ru/creating-a-react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Перейти к статье
         </a>
       </header>
     </div>
